370702f0faf4e5d95188a5ea1c707d5be5adcbb8
M. Dzikri <153479512+bankri@users.noreply.github.com>
2025-05-26 10:01:31 +0700

    Add files via upload

---
+++ PORTFOLIO/CODE/database.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCCOYpYyItY7vMMf8nAxxvm_rT4RJHHikw",
+    authDomain: "myporto-c9e69.firebaseapp.com",
+    projectId: "myporto-c9e69",
+    storageBucket: "myporto-c9e69.firebasestorage.app",
+    messagingSenderId: "666144042242",
+    appId: "1:666144042242:web:e5ae82984799eed900261b"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
